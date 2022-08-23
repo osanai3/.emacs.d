981d3a9b39a8aa2f86d8bc7f717211170728eba2
Koichi Osanai <osanai3@gmail.com>
2022-08-24 01:53:34 +0900

    remove consult

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
      ("gnu" . "https://elpa.gnu.org/packages/")))
  '(package-quickstart t)
  '(package-selected-packages
-   '(protobuf-mode rust-mode renda go-mode company eglot multi-vterm vterm embark-consult embark marginalia orderless icomplete-vertical consult pipe-to-emacsclient swap-buffer typescript-mode dockerfile-mode yaml-mode purescript-mode exec-path-from-shell restart-emacs js2-mode markdown-mode haskell-mode))
+   '(protobuf-mode rust-mode renda go-mode company eglot multi-vterm vterm embark marginalia orderless icomplete-vertical pipe-to-emacsclient swap-buffer typescript-mode dockerfile-mode yaml-mode purescript-mode exec-path-from-shell restart-emacs js2-mode markdown-mode haskell-mode))
  '(recentf-max-saved-items 1000)
  '(require-final-newline t)
  '(revert-without-query '(".*"))
@@ -155,20 +155,13 @@
  )
 
 (icomplete-mode)
-;;(if (fboundp 'icomplete-vertical-mode) (icomplete-vertical-mode))
 (fido-vertical-mode)
 (defvar icomplete-minibuffer-map)
 (define-key icomplete-minibuffer-map "\t" 'icomplete-force-complete)
 (define-key icomplete-minibuffer-map "\C-n" 'icomplete-forward-completions)
 (define-key icomplete-minibuffer-map "\C-p" 'icomplete-backward-completions)
-(global-set-key (kbd "M-g M-g") 'consult-goto-line)
-;(global-set-key (kbd "C-s") 'consult-isearch-history)
-(global-set-key (kbd "C-x b") 'consult-buffer)
 (setq completion-styles '(orderless))
 (global-set-key (kbd "C-;") 'embark-act)
-(with-eval-after-load 'consult
-  (with-eval-after-load 'embark
-    (require 'embark-consult)))
 (global-set-key (kbd "C-z") 'vterm)
 
 (with-eval-after-load 'vterm
