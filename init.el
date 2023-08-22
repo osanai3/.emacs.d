e18f3c55688eb996df9af27319a4ffbb2f25da82
Koichi Osanai <osanai3@gmail.com>
2023-08-22 22:35:38 +0900

    remove eglot

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
      ("gnu" . "https://elpa.gnu.org/packages/")))
  '(package-quickstart t)
  '(package-selected-packages
-   '(corfu quelpa protobuf-mode rust-mode renda go-mode eglot multi-vterm vterm embark marginalia orderless icomplete-vertical pipe-to-emacsclient swap-buffer dockerfile-mode yaml-mode purescript-mode exec-path-from-shell js2-mode markdown-mode haskell-mode))
+   '(corfu quelpa protobuf-mode rust-mode renda go-mode multi-vterm vterm embark marginalia orderless icomplete-vertical pipe-to-emacsclient swap-buffer dockerfile-mode yaml-mode purescript-mode exec-path-from-shell js2-mode markdown-mode haskell-mode))
  '(recentf-max-saved-items 1000)
  '(require-final-newline t)
  '(revert-without-query '(".*"))
@@ -179,7 +179,7 @@
   (define-key flymake-mode-map (kbd "M-p") 'flymake-goto-prev-error))
 
 (add-hook 'emacs-lisp-mode-hook 'flymake-mode)
-;;(add-hook 'typescript-ts-base-mode-hook 'eglot-ensure)
+(add-hook 'typescript-ts-base-mode-hook 'eglot-ensure)
 (add-hook 'go-mode-hook 'eglot-ensure)
 (add-hook 'rust-mode-hook 'eglot-ensure)
 (add-hook 'ruby-mode-hook 'eglot-ensure)
@@ -206,10 +206,6 @@
       (progn
         (define-key haskell-mode-map ">" (renda-str '(">" "->" "=>")))
         (define-key haskell-mode-map "<" (renda-str '("<" "<-"))))))
-
-(with-eval-after-load 'eglot
-  (defvar eglot-server-programs)
-  (push '(typescript-mode . ("typescript-language-server" "--stdio")) eglot-server-programs))
 
 (if (fboundp 'renda-pos)
     (progn
