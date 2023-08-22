4aaef76236a0f4ff1bfa3279da0f303779b3aff7
Koichi Osanai <osanai3@gmail.com>
2023-08-22 21:53:16 +0900

    remove restart-emacs

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
      ("gnu" . "https://elpa.gnu.org/packages/")))
  '(package-quickstart t)
  '(package-selected-packages
-   '(corfu quelpa protobuf-mode rust-mode renda go-mode eglot multi-vterm vterm embark marginalia orderless icomplete-vertical pipe-to-emacsclient swap-buffer typescript-mode dockerfile-mode yaml-mode purescript-mode exec-path-from-shell restart-emacs js2-mode markdown-mode haskell-mode))
+   '(corfu quelpa protobuf-mode rust-mode renda go-mode eglot multi-vterm vterm embark marginalia orderless icomplete-vertical pipe-to-emacsclient swap-buffer typescript-mode dockerfile-mode yaml-mode purescript-mode exec-path-from-shell js2-mode markdown-mode haskell-mode))
  '(recentf-max-saved-items 1000)
  '(require-final-newline t)
  '(revert-without-query '(".*"))
